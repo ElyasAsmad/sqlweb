3c8efd373c123b1866d5ad5bab23a94288ea7bbd
Elyas Asmad <elyasasmadz@gmail.com>
2023-01-25 17:15:57 +0800

    🐛 Baiki bug font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,16 +6,16 @@ const theme = createTheme({
     typography: {
         fontFamily: 'Satoshi, sans-serif'
     },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-                @font-face {
-                    font-family: 'Satoshi';
-                    src: https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&display=swap
-                }
-            `
-        }
-    }
+    // components: {
+    //     MuiCssBaseline: {
+    //         styleOverrides: `
+    //             @font-face {
+    //                 font-family: 'Satoshi';
+    //                 src: https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,300,400&display=swap
+    //             }
+    //         `
+    //     }
+    // }
 })
 
 export default function App({ Component, pageProps }: AppProps) {
